ad7e1fc2e2666f0a7be9797d02946d151ea07e25
Arnold S.A. Chui <asachui1@gmail.com>
2014-11-30 15:09:27 +0000

    Use annotations instead of web.xml

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import models.Authenticator;
 import models.User;
 
+@WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
